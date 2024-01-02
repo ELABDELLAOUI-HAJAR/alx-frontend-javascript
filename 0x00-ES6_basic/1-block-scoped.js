34b7e3de814a4f9e36d07776aa762f8afd174a18
HAJAR <elabdellaoui.hajar@gmail.com>
2024-01-02 23:12:04 +0100

    task 1 taskBlock: var and hoisting

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
