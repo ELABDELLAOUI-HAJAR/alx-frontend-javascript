5496152696548cf33714a1339f0de22dca409361
HAJAR <elabdellaoui.hajar@gmail.com>
2024-01-10 20:02:40 +0100

    task 1: create Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,8 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
