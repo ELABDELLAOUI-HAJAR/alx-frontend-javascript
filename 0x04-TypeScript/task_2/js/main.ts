15939600b362ec7caa567b797e5b35e04d697771
HAJAR <elabdellaoui.hajar@gmail.com>
2024-01-10 21:57:43 +0100

    task 7: use type and use it in a function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -51,3 +51,12 @@ function executeWork(employee: Director | Teacher) {
   }
   return (employee as Teacher).workTeacherTasks();
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
+}
